5347fc83203b4e9bf12fe214189fded0ef58b2ba
Peng <nj.pengzhang@gmail.com>
2012-12-31 17:36:39 -0800

    Edit Hero Unit

--- js/drone.js
+++ js/drone.js
@@ -2,5 +2,19 @@
  * @author Peng Zhang
  * 2012
  */
+(function() {
+	var hero_info = $("#hero_info");
 
- $("#header").html("git cat");
+	hero_info.popover({
+		animation: true,
+		placement: "bottom",
+		html: true,
+		selector: true,
+		trigger: "hover",
+		title: "Pasha Liman",
+		content: "<small>Pasha Liman is an Albanian navy base South Albania.</small>"
+	});
+	// hero_info.click(function(event) {
+	// 	console.log(event);
+	// });
+} ());
