42f4208bfff4923626750362854c88eafd25058a
Peng <nj.pengzhang@gmail.com>
2012-12-31 23:48:03 -0800

    Clean up

--- js/drone.js
+++ js/drone.js
@@ -5,16 +5,5 @@
 (function() {
 	var hero_info = $("#hero_info");
 
-	hero_info.popover({
-		animation: true,
-		placement: "bottom",
-		html: true,
-		selector: true,
-		trigger: "hover",
-		title: "Pasha Liman",
-		content: "<small>Pasha Liman is an Albanian navy base South Albania.</small>"
-	});
-	// hero_info.click(function(event) {
-	// 	console.log(event);
-	// });
+	//hero_info.modal("show");
 } ());
