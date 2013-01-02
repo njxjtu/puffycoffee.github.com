22cf00894eb863c0cd600d4dcc3bd8e14627c131
Peng <nj.pengzhang@gmail.com>
2013-01-01 22:13:55 -0800

    Front page

--- js/drone.js
+++ js/drone.js
@@ -4,7 +4,7 @@
  */
 (function() {
 	var hero_info = $("#hero_info"),
-		paper = Raphael("game-paper", 300, 180);
+		paper = Raphael("game-paper", 300, 130);
 	paper.rect(5, 5, 40, 24, 5).attr({
 		fill: '#33ccff'
 	});
