f84df80264a60a20b55247cab2ba3eb1ee198db8
Peng <nj.pengzhang@gmail.com>
2013-08-09 11:31:53 -0700

    keep tweaking

--- js/drone.js
+++ js/drone.js
@@ -3,14 +3,24 @@
  * 2012
  */
 (function() {
+	var pic_me = $("#pic");
+	//pic_me.draggable();
+	console.log(pic_me);
+	console.log(pic_me.draggable());
 	var translator = $("#translate");
-	translator.click(function() {
-		$("#aboutme").flip({
-			direction:'rl',
-			color: '#f5f5f5',
-			content: '<div>Hello, My name is Peng Zhang, I am a Java front-end developer working in El Dorado Hills, California. I spend 60% of my    time design custom JSF component. When I am not working I    can be found writing open source projects or playing video  games.'
-		});
-		$("#translate").html("Back to nerdy world.");
-	});
+	translator.toggle(
+		function() {
+			$("#aboutme").flip({
+				direction:'rl',
+				color: '#f5f5f5',
+				content: '<div>Hello, My name is Peng Zhang, I am a Java front-end developer working in El Dorado Hills, California. I spend 60% of my    time design custom JSF component. When I am not working I    can be found writing open source projects or playing video  games.'
+			});
+			translator.html("Back to nerdy world.");
+		},
+		function() {
+			$("#aboutme").revertFlip();
+			translator.html("Could you speak English??");
+		}
+	);
 	
 } ());
