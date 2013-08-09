bfeac16e86350491dfa3efa0f2f5d01e30576e51
Peng <nj.pengzhang@gmail.com>
2013-08-09 11:09:14 -0700

    tweak

--- js/drone.js
+++ js/drone.js
@@ -8,7 +8,7 @@
 		$("#aboutme").flip({
 			direction:'rl',
 			color: '#f5f5f5',
-			content: '<div>Hello, My name is Peng Zhang, I am a Java front-end developer working in El Dorado Hills, California. I spend 60% of my time design custom JSF component. When I am not working I can be found writing open source projects or playing video games.'
+			content: '<div>Hello, My name is Peng Zhang, I am a Java front-end developer working in El Dorado Hills, California. I spend 60% of my    time design custom JSF component. When I am not working I    can be found writing open source projects or playing video  games.'
 		});
 		$("#translate").html("Back to nerdy world.");
 	});
